94948b821d2e6ca9a26a53a451b05f9c4412bb69
Hasan Goli <hasangolidev@gmail.com>
2024-06-19 21:57:30 +0330

    Mongoose and mongodb configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    CoffeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
